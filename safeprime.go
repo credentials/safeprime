cad0a6cd24982ccf91a5e37bcad48a157b1f7c86
Sietse Ringers <mail@sietseringers.net>
2017-05-06 12:37:16 +0200

    Use only golang

--- safeprime.go
+++ safeprime.go
@@ -3,10 +3,7 @@ package safeprime
 
 import (
 	"crypto/rand"
-	"errors"
 	"math/big"
-
-	"github.com/rainycape/dl"
 )
 
 var bnNew func() uintptr
@@ -17,62 +14,14 @@ var bnToHex func(uintptr) string
 // Generate tries to use openssl's BN_generate_prime_ex to generate a new safe prime of the given size;
 // if that fails it uses a pure Go algorithm.
 func Generate(bitsize int) (*big.Int, error) {
-	num, err := GenUsingOpenssl(bitsize)
+	// num, err := GenUsingOpenssl(bitsize)
+	//
+	// if err != nil {
+	// 	println("WARNING: using openssl failed, switching to (slower) Go algorithm")
+	return GenUsingGo(bitsize)
+	// }
 
-	if err != nil {
-		println("WARNING: using openssl failed, switching to (slower) Go algorithm")
-		return GenUsingGo(bitsize)
-	}
-
-	return num, nil
-}
-
-// GenUsingOpenssl uses openssl's BN_generate_prime_ex to generate a new safe prime of the given size.
-func GenUsingOpenssl(bitsize int) (*big.Int, error) {
-	openssl, err := linkOpenssl()
-	if err != nil {
-		return nil, err
-	}
-	defer openssl.Close()
-
-	bignum := bnNew()
-	if bignum == 0 {
-		return nil, errors.New("BN_new could not allocate new bignum")
-	}
-	defer bnFree(bignum)
-
-	if r := bnGenPrime(bignum, bitsize, 1, 0, 0, 0); r != 1 {
-		return nil, errors.New("BN_generate_prime_ex failed")
-	}
-
-	x := new(big.Int)
-	x.SetString(bnToHex(bignum), 16)
-	return x, nil
-}
-
-func linkOpenssl() (*dl.DL, error) {
-	openssl, err := dl.Open("libssl", 0)
-	if err != nil {
-		return nil, err
-	}
-
-	if err = openssl.Sym("BN_new", &bnNew); err != nil {
-		return nil, err
-	}
-
-	if err = openssl.Sym("BN_clear_free", &bnFree); err != nil {
-		return nil, err
-	}
-
-	if err = openssl.Sym("BN_generate_prime_ex", &bnGenPrime); err != nil {
-		return nil, err
-	}
-
-	if err = openssl.Sym("BN_bn2hex", &bnToHex); err != nil {
-		return nil, err
-	}
-
-	return openssl, nil
+	// return num, nil
 }
 
 // GenUsingGo generates a safe prime in pure Go.
